8a4ba8b59738241ff1463faf31dfa431852968ae
Anthony Anderson <anderson@mdhs.info>
2020-07-01 01:45:53 -0400

    Inital commit for fg rewrite
    Signed-off-by: Anthony Anderson <anderson@mdhs.info>

---
+++ mcp/src/main/java/net/minecraftforge/gradle/mcp/MCPPlugin.java
@@ -0,0 +1,12 @@
+package net.minecraftforge.gradle.mcp;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class MCPPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+
+    }
+}
---
+++ userdev/src/main/java/net/minecraftforge/gradle/userdev/UserDevPlugin.java
@@ -0,0 +1,12 @@
+package net.minecraftforge.gradle.userdev;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class UserDevPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+
+    }
+}
